1118a9f49a8e93064dc1681f1b42185890f0a75a
vinay h <140433857+Vinayh96@users.noreply.github.com>
2025-06-30 13:46:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
  public static void main(String args[])
  {
-  System.out.println("Hello world and welcome");
+  System.out.println("Hello world and welcome made some changes");
   }
   }
